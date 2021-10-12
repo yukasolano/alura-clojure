d7e7e08805e2c7c2d7aeedb354ceda1c0441feb3
Yuka Solano <yukakyushima@gmail.com>
2021-10-12 16:35:45 -0300

    Curso 7 - Aula 3

--- test/curso7/logic_test.clj
+++ test/curso7/logic_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [curso7.logic :refer :all]
             [schema.core :as s]
+            [curso7.model :as h.model]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.properties :as prop]))
@@ -36,3 +37,31 @@
                         pessoa gen/string-alphanumeric]
                        (is (= {:espera (conj fila pessoa)}
                               (chega-em {:espera fila} :espera pessoa)))))
+(defn transforma-vector-em-fila [vector]
+  (reduce conj h.model/fila-vazia vector))
+
+(def nome-aleatorio-gen
+  (gen/fmap clojure.string/join (gen/vector gen/char-alpha 5 10)))
+
+(def fila-nao-cheia-gen
+  (gen/fmap transforma-vector-em-fila
+            (gen/vector nome-aleatorio-gen 1 4)))
+(defn total-de-pacientes
+  [hospital]
+  (->> hospital
+       vals
+       (map count)
+       (reduce +)))
+
+;testa a propriedade do nosso codigo de manter o numero de pessoas no transfere
+(defspec transfere-tem-que-manter-a-quantidade-de-pessoas 5
+         (prop/for-all
+          [espera fila-nao-cheia-gen
+           raio-x fila-nao-cheia-gen
+           ultrasom fila-nao-cheia-gen
+           vai-para (gen/elements [:raio-x :ultrasom])
+           ]
+          (let [hospital-inicial {:espera espera :raio-x raio-x :ultrasom ultrasom}
+                hospital-final (transfere hospital-inicial :espera vai-para)]
+            (= (total-de-pacientes hospital-inicial)
+               (total-de-pacientes hospital-final)))))
